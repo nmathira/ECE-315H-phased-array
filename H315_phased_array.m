1f751e09de588844c80c32571f41af54325a2088
davisdriving <ondavis@umass.edu>
2025-11-08 21:03:56 -0500

    added spacing variation, restructuring, comment :q :q!

--- H315_phased_array.m
+++ H315_phased_array.m
@@ -1,34 +1,45 @@
 % Niranjan Mathirajan, Owen Davis
-% October 14, 2025
+% November 8, 2025
 % ECE 315H MATLAB Phased Array Antenna 
+% Source 1: https://innovationspace.ansys.com/courses/wp-content/uploads/sites/5/2022/07/AntennaArrays_04ManipulatingtheArrayFactor.pdf
+% Source 2: https://www.antenna-theory.com/arrays/arrayfactor.php
+% Source 3: https://www.antenna-theory.com/definitions/wavevector.php
+
+clc;
+clf;
+
+% -- constants --
+f = 5e6; % frequency of antenna (Hz)
+c = 3e8; % speed of light (m/s)
+lambda = c/f; % wavelength (m)
+k = 2 * pi/lambda; % 1-D wave vector, see source (3) (rad/m)
 
 
-% https://innovationspace.ansys.com/courses/wp-content/uploads/sites/5/2022/07/AntennaArrays_04ManipulatingtheArrayFactor.pdf
+% -- array parameters --
+N = 8; % number of antennas
+nTheta = 360; 
+theta = linspace(-pi/2, pi/2, nTheta); % range of angles the array "sees"
+steerAngle = 0 * pi/180; % steering angle of array
+dx_ideal = 0.5 * lambda; % expected distance between antennas
+dx_range = .1 * lambda; % max variation in spacing
+dx_actual = (dx_ideal-dx_range) + (dx_range + dx_range).*rand(1,N); % compute random spacing
+phase = -k * dx_ideal * cos(steerAngle); % term added to each AF to aim beam
 
-f = 5e6; % frequency of antenna 
-c = 3e8; % speed of light
-lambda = c/f;
-dx = 0.25 * lambda; % distance between antennas
-N = 10; % number of antennas
-nTheta = 180;
-theta = linspace(-pi/2, pi/2, nTheta);
-steerAngle = 0 * pi/180; % direction of anetanna figure out this phase stuff later
-phase = -k * dx * cos(steerAngle);
-AF = zeros(nTheta,1);
-
-% Array Factor value
+% -- compute array factor -- 
+AF = zeros(nTheta,1); 
 for t = 1:nTheta
     for n = 1:N
-        AF(t) = AF(t) + exp(1j*(n-1)*(k*dx*cos(theta(t)) + phase));
+        % AF equation from source (1):
+        AF(t) = AF(t) + exp(1j*(n-1)*(k*dx_actual(n)*cos(theta(t)) + phase)); 
     end
-    AF(t) = AF(t)/N;
+    AF(t) = AF(t)/N; % normalize AF, so max gain is 0dB
 end
 
-
-polarplot(theta, abs(AF),"blue",...
-    'LineWidth',2)
-
+% -- plotting --
+hps = polaraxes;
+polarplot(theta, abs(AF),"blue", 'LineWidth',2)
+hps.ThetaZeroLocation = 'top'; % make 0 degrees show up at top of plot
 ax = gca; 
 gca.FontSize = 12;
-
-grid on
+title('Normalized Array Factor (dB)');
+grid on;
