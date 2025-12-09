b858753fcaf2a649e0b2aa21327f6be8dbc4c0e1
nmathira <mathirajanniranjan@gmail.com>
2025-12-08 21:33:38 -0500

    refactor: improved comments, with important notes regarding design

--- H315_phased_array.m
+++ H315_phased_array.m
@@ -5,52 +5,59 @@
 % Source 2: https://www.antenna-theory.com/arrays/arrayfactor.php
 % Source 3: https://www.antenna-theory.com/definitions/wavevector.php
 
+
+% Some notes about the simulation:
+% the phase factor should always assume ideal spacing, as the computer
+% doesn't know about the error in spacing. This is why the simulation and
+% calculation's spacing don't incorporate the actual spacing, rather the
+% ideal
+%
+% The expected calculation should never use the actual spacing, just ideal
+% and error. The whole point is that the expected value incorporates the
+% ideal distribution of dx_error (uniform), so that we can calculate the
+% true error.
 clc;
 clf;
 
 % -- constants --
+T = 100; % number of trials performed
 f = 5e6; % frequency of antenna (Hz)
 c = 3e8; % speed of light (m/s)
 lambda = c/f; % wavelength (m)
 k = 2 * pi/lambda; % 1-D wave vector, see source (3) (rad/m)
-T = 20000; % number of trials performed
 
 % -- array parameters --
 N = 16; % number of antennas
-nTheta = 180; 
+nTheta = 180;
 theta = linspace(-pi/2, pi/2, nTheta); % range of angles the array "sees"
-steerAngle = 50 * pi/180; % steering angle of array
+steerAngle = 30 * pi/180; % steering angle of array
 dx_ideal = 0.25 * lambda; % expected distance between antennas
 dx_error = 0.05 * lambda; % max variation in spacing
-% 
-% dx_actual = zeros(N,1);
-% for n = 1:N
-%     dx_actual(n) = dx_ideal - dx_error + 2*dx_error*rand();
-% end
-% 
-% pct_error = abs(dx_actual - dx_ideal)/dx_ideal * 100;
-% disp(pct_error);
 
 phase = -k * dx_ideal * cos(steerAngle); % term added to each AF to aim beam
-measure_angle_deg = 50; % angle we measure AF at (range from 1 to 90)
+measure_angle_deg = 30; % angle we measure AF at (range from 1 to 90)
 measure_angle_rad = measure_angle_deg * 2*pi / 360;
 
+% --- Monte Carlo Simulation ---
+
 simulated_average = zeros(T,1);
-
-% -- compute array factor -- NO RANDOM SPACING
-
 for i = 1:T
-    AF = zeros(nTheta,1); 
+    AF = zeros(nTheta,1);
+
+    % --- get spacings ---
     dx_actual = zeros(N,1);
     for n = 1:N
         dx_actual(n) = dx_ideal - dx_error + 2*dx_error*rand();
     end
 
-
+    % --- get Array Factor for every angle
     for t = 1:nTheta
         for n = 1:N
             % AF equation from source (1):
-            AF(t) = AF(t) + exp(1j*(n-1)*(k*dx_actual(n)*cos(theta(t)) - k*dx_ideal*cos(steerAngle))); 
+            main_term = k*dx_actual(n)*cos(theta(t));
+            phase_term = - k*dx_ideal*cos(steerAngle);
+
+            AF(t) = AF(t) + exp(1j*(n-1)*(main_term + phase_term));
         end
         AF(t) = AF(t)/N; % normalize AF, so max gain is 0dB
     end
@@ -59,14 +66,22 @@ end
 
 simulated_average = mean(simulated_average)
 
+% --- Expected calculation ---
+
 E = 0;
 for n = 1:N
     m = n-1;
-    main_term = exp(1j*k*(n-1)*cos(measure_angle_rad)*dx_ideal);
-    phase_term = exp(-1j*k*(n-1)*dx_ideal*cos(steerAngle));
+    % these terms were pulled out of the array factor, should match
+    % previous.
+    main_term = exp(1j*k*m*cos(measure_angle_rad)*dx_ideal);
+    phase_term = exp(-1j*k*m*dx_ideal*cos(steerAngle));
 
     if m == 0
-        random_variable = 1;   % when working at 0 degrees, the denominator blows up, causing errors.
+        % when working at 0th antenna, the random variable doesn't work.
+        % I'm guessing it's because there's no randomness in it's
+        % placement? The practical reason is that the n-1 term causes a 0
+        % in the numerator and denominator.
+        random_variable = 1;
     else
         random_variable = (exp(1j*k*m*cos(measure_angle_rad)*dx_error) ...
             - exp(1j*k*m*cos(measure_angle_rad)*-1*dx_error))./(1j*k*m*cos(measure_angle_rad)*2*dx_error);
